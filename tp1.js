3ff03181418ea116e07e819356e02377b4cb531d
MaxiIvan <85696496+maxiivandlf@users.noreply.github.com>
2023-06-22 01:09:00 -0300

    Ejercicio_1_resuelto

--- tp1.js
+++ tp1.js
@@ -6,7 +6,8 @@
 */
 
 function extractValues(data) {
-  // Tu código aquí
+  const valuesObject = Object.values(data);
+  return valuesObject;
 }
 
 /*  
@@ -36,9 +37,9 @@ function findKeysInCommon(objeto1, objeto2) {
 // Llamamos a las funciones y mostramos los resultados
 
 function main() {
-  console.log(extractValues({a: 1, b: 2, c: 3}));
+  console.log(extractValues({ a: 1, b: 2, c: 3 }));
   console.log(arrayToLengthCounters(['gato', 'perro', 'elefante']));
-  console.log(findKeysInCommon({a: 1, b: 2, c: 3}, {b: 4, c: 5, d: 6}));
+  console.log(findKeysInCommon({ a: 1, b: 2, c: 3 }, { b: 4, c: 5, d: 6 }));
 }
 
 main();
